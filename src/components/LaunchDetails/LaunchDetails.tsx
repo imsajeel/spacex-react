b71b461cdc11824d14941d93c08ab458f071405b
Sajeel Aalam <imsajeel.dev@gmail.com>
2021-02-01 12:35:26 +0500

    Image added in details

--- src/components/LaunchDetails/LaunchDetails.tsx
+++ src/components/LaunchDetails/LaunchDetails.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@geist-ui/react";
+import { Card, Table } from "@geist-ui/react";
 import React from "react";
 import { LaunchInfoQuery } from "../../generated/graphql";
 
@@ -11,6 +11,13 @@ const LaunchDetails: React.FC<Props> = ({ data }) => {
 
   const { launch } = data;
 
+  const tableData = [
+    { label: "Flight number", value: launch?.flight_number },
+    { label: "Launch site", value: launch?.launch_site?.site_name },
+    { label: "Launch success", value: `${launch?.launch_success}` },
+    { label: "Launch year", value: launch?.launch_year },
+  ];
+
   return (
     <div style={{ margin: "10px" }}>
       <h3 style={{ margin: "5vh 0px", textAlign: "center" }}>Launch Details</h3>
@@ -18,6 +25,35 @@ const LaunchDetails: React.FC<Props> = ({ data }) => {
         <h4>
           {launch?.mission_name} - {launch?.launch_year}
         </h4>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap-reverse",
+            marginTop: "40px",
+          }}
+        >
+          <Table data={tableData} style={{ maxWidth: "300px" }}>
+            <Table.Column prop="label" label="Label" />
+            <Table.Column prop="value" label="Value" />
+          </Table>
+          {!!launch?.links?.flickr_images && (
+            <div
+              style={{
+                maxWidth: "250px",
+                maxHeight: "250px",
+                margin: "0px 10px 10px auto",
+                marginLeft: "auto",
+                borderRadius: "5px",
+                overflow: "hidden",
+              }}
+            >
+              <img
+                src={launch?.links?.flickr_images.toString()}
+                alt={launch.mission_name?.toString()}
+              />
+            </div>
+          )}
+        </div>
         <p>{launch?.details}</p>
       </Card>
     </div>
